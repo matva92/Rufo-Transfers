9f2806b57eb859961cfef5e7f894627699b8bcac
Matias Rufino Varela <mvarela@Lenovo.com>
2020-05-23 18:44:24 -0300

    reloj

--- public/css/loader.js
+++ public/css/loader.js
@@ -1,5 +1,8 @@
-$(window).on("load",function(){
-   
-    setTimeout(function(){ $(".loader-wrapper").fadeOut("slow") }, 1000);
-    
+$(window).on("load", function () {
+
+    startTime()
+    setTimeout(function () {
+        $(".loader-wrapper").fadeOut("slow")
+    }, 1000);
+
 });
