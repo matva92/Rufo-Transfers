b7725163cbb4a7e156b485874706db0779239aea
Matias Rufino Varela <mvarela@Lenovo.com>
2020-05-24 01:11:51 -0300

    mobile cotizar function

---
+++ formulario_toggle.js
@@ -0,0 +1,10 @@
+$("li .cotizar").click(function() {  
+    // console.log("testing");
+    $(".formulario").toggleClass("hidden");
+});
+
+$("li .cotizar2").click(function() {  
+    // console.log("testing");
+    $(".formulario").toggleClass("hidden");
+    $(".toggler").prop("checked", false);
+});
